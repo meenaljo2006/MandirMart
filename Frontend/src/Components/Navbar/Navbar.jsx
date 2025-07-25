fde524f3f1c39397582170caab3febcabb7446f2
meenaljo2006 <meenaljoshi2006@gmail.com>
2025-07-25 20:11:23 +0530

    UILogic-LoginAdded

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -30,8 +30,10 @@ const Navbar = ()=>{
                 <li onClick={()=>{setMenu("idols")}}><Link style={{textDecoration:"none"}} to="/idols">Idols</Link>{menu==="idols"?<hr/>:<></>}</li>
                 <li onClick={()=>{setMenu("puja")}}><Link style={{textDecoration:"none"}} to="/puja">Puja Samagri</Link>{menu==="puja"?<hr/>:<></>}</li>
             </ul>
-            <div className="nav-login-cart">
-                <Link to="/login"><button>Login</button></Link>
+            <div className="nav-login-cart">\
+                {localStorage.getItem("auth-token")?<button onClick={()=>{localStorage.removeItem("auth-token"); window.location.replace("/")}}>Logout</button>
+                :<Link to="/login"><button>Login</button></Link>}
+                
                 <Link to="/cart"><img src={cart_icon} alt=""/></Link>
                 <div className="nav-cart-count">{getTotalCartItems()}</div>
             </div>
